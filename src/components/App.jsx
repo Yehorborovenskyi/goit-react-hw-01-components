6bf86b3a53590464eff9ba24be1e3473bc68e909
Yehorborovenskyi <yehorborovenskyi@gmail.com>
2023-04-08 20:36:07 +0300

    Profile create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,16 @@
-export const App = () => {
+export const App = ({ children }) => {
   return (
-    <div>App
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      {children}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { Profile } from 'components/Profile/Profile';
+import user from './components/Profile/user.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </App>
   </React.StrictMode>
 );
